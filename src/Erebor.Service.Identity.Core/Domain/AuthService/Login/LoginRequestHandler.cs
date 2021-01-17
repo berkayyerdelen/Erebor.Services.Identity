a0a6dbe85a004e813c12dc57dde1042575b1a6ab
Berkay <Berkay@DESKTOP-ULLAD9F>
2021-01-17 21:55:29 +0300

    -imp loginRequestHandler -PasswordHelper

--- src/Erebor.Service.Identity.Core/Domain/AuthService/Login/LoginRequestHandler.cs
+++ src/Erebor.Service.Identity.Core/Domain/AuthService/Login/LoginRequestHandler.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
+using Erebor.Service.Identity.Core.Exceptions;
 using Erebor.Service.Identity.Domain.Repositories;
+using Erebor.Service.Identity.Shared.Security;
 using MediatR;
 
 namespace Erebor.Service.Identity.Core.Domain.AuthService.Login
@@ -16,6 +20,17 @@ namespace Erebor.Service.Identity.Core.Domain.AuthService.Login
         public async Task<LoginResult> Handle(LoginRequest request, CancellationToken cancellationToken)
         {
             var user =await _userRepository.GetUserAsync(request.UserName);
+            var isValidPassword = PasswordHelper.Check(user.Password, request.Password);
+            if (!isValidPassword)
+                throw new ServiceException("Incorrect Password");
+            var roles = user.Roles;
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName)
+            };
+            roles.ForEach(role => { claims.Add(new Claim(ClaimTypes.Role, role.Value)); });
+
+            //Todo:Add Token Generetor
 
             return null;
         }
---
+++ src/Erebor.Service.Identity.Core/Exceptions/ServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Erebor.Service.Identity.Core.Exceptions
+{
+    public class ServiceException : Exception
+    {
+        public ServiceException(string empty):base(empty)
+        {
+            
+        }
+    }
+}
